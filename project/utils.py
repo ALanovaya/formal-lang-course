5668b22096c50eed988e611e63d947637b53f907
ALanovaya <lanovaya.al6@gmail.com>
2024-09-07 23:29:13 +0300

    Format files

--- project/utils.py
+++ project/utils.py
@@ -2,6 +2,7 @@ from typing import Tuple, Set
 import cfpq_data
 import networkx as nx
 
+
 class Graph:
     def __init__(self, node_count: int, edge_count: int, edge_labels: Set[str]):
         self.node_count = node_count
@@ -15,22 +16,19 @@ class Graph:
             and self.edge_labels == other.edge_labels
         )
 
+
 def get_graph(graph_name: str) -> Graph:
     graph = cfpq_data.download(graph_name)
     graph = cfpq_data.graph_from_csv(graph)
     edge_labels = {data["label"] for _, _, data in graph.edges.data()}
     return Graph(graph.number_of_nodes(), graph.number_of_edges(), edge_labels)
 
+
 def lift_labeled_two_cycles_graph_to_dot(
-    first_cycle_size: int,
-    second_cycle_size: int,
-    labels: Tuple[str, str],
-    path: str
+    first_cycle_size: int, second_cycle_size: int, labels: Tuple[str, str], path: str
 ) -> None:
     graph = cfpq_data.labeled_two_cycles_graph(
-        n=first_cycle_size,
-        m=second_cycle_size,
-        labels=labels
+        n=first_cycle_size, m=second_cycle_size, labels=labels
     )
     pydot_graph = nx.drawing.nx_pydot.to_pydot(graph)
     with open(path, "w") as f:
